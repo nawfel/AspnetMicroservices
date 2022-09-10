3db54a17cc80d52a15d7ba4c6cc306c2641f6136
nawfel <sghaier.nawfel@hotmail.com>
2022-09-10 17:14:30 +0200

    serilog

--- src/WebApps/AspnetRunBasics/Services/CatalogService.cs
+++ src/WebApps/AspnetRunBasics/Services/CatalogService.cs
@@ -21,7 +21,7 @@ namespace AspnetRunBasics.Services
 
         public async Task<IEnumerable<CatalogModel>> GetCatalog()
         {
-            _logger.LogInformation("Getting catalog products from url {0}:",_client.BaseAddress);
+            _logger.LogInformation("Getting catalog products from url : {0} and custom property : {cutomProperty} ",_client.BaseAddress,6);
             var response = await _client.GetAsync("/Catalog");
             return await response.ReadContentAs<List<CatalogModel>>();
         }
